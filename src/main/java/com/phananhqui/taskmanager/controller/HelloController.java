ab95f89b53ad5d887ac3b2bc6b9d0adb55401116
Qui <quiphanh@gmail.com>
2025-10-29 23:35:45 +0700

    prepare for render deploy

---
+++ src/main/java/com/phananhqui/taskmanager/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.phananhqui.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Spring Boot 3 + Swagger + Java 21 🚀";
+    }
+}
